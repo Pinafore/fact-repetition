b4496a1f6e689e2f644faa13026c2f445d01bc8e
DenisPeskov <dap87@georgetown.edu>
2019-06-17 13:44:45 -0400

    Update qanta.py
    Updated qanta data to contain a "question_stats" field.  (Updated data currently found in /fs/clip-quiz/flashcards/data).   This data is then read into AllenNLP.   Did not actually run but don't see obvious typo.  
    
    Lines 74 and 76 need check as I don't know syntax to say that data type is List of floats.

--- backend/src/fact/datasets/qanta.py
+++ backend/src/fact/datasets/qanta.py
@@ -23,9 +23,9 @@ from fact.util import get_logger
 
 log = get_logger(__name__)
 
-QANTA_TRAIN = 'data/qanta.train.2018.04.18.json'
-QANTA_DEV = 'data/qanta.dev.2018.04.18.json'
-QANTA_TEST = 'data/qanta.test.2018.04.18.json'
+QANTA_TRAIN = 'data/expanded.qanta.train.2018.04.18.json'
+QANTA_DEV = 'data/expanded.qanta.dev.2018.04.18.json'
+QANTA_TEST = 'data/expanded.qanta.test.2018.04.18.json'
 
 
 @DatasetReader.register('qanta')
@@ -52,11 +52,17 @@ class QantaReader(DatasetReader):
                     if self._break_questions:
                         for start, end in q['tokenizations']:
                             sentence = q['text'][start:end]
-                            instance = self.text_to_instance(sentence, answer=q['page'], qanta_id=q['qanta_id'])
+                            instance = self.text_to_instance(sentence, answer=q['page'], qanta_id=q['qanta_id'], 
+                                                             length_ratios = q['question_stats']['length_per_question'],
+                                                             overall_accuracy = q['question_stats']['accuracy_per_question'],
+                                                             individual_accuracy = q['question_stats']['accuracy_bools_per_question'])
                             if instance is not None:
                                 yield instance
                     else:
-                        instance = self.text_to_instance(q['text'], answer=q['page'], qanta_id=q['qanta_id'])
+                        instance = self.text_to_instance(q['text'], answer=q['page'], qanta_id=q['qanta_id'],
+                                                        length_ratios = q['question_stats']['length_per_question'],
+                                                        overall_accuracy = q['question_stats']['accuracy_per_question'],
+                                                        individual_accuracy = q['question_stats']['accuracy_bools_per_question'])
                         if instance is not None:
                             yield instance
 
@@ -64,7 +70,11 @@ class QantaReader(DatasetReader):
     def text_to_instance(self,
                          text: str,
                          answer: str = None,
-                         qanta_id: int = None):
+                         qanta_id: int = None,
+                         length_ratios: List[float] = None,
+                         overall_accuracy: float = None,
+                         individual_accuracy: List[float] = None 
+                        ):
         fields: Dict[str, Field] = {}
         tokenized_text = self._tokenizer.tokenize(text)
         if len(tokenized_text) == 0:
@@ -72,5 +82,7 @@ class QantaReader(DatasetReader):
         fields['text'] = TextField(tokenized_text, token_indexers=self._token_indexers)
         if answer is not None:
             fields['answer'] = LabelField(answer, label_namespace='answer_labels')
-        fields['metadata'] = MetadataField({'qanta_id': qanta_id})
+        fields['metadata'] = MetadataField({'qanta_id': qanta_id, 'length_ratios':length_ratios,
+                                           'overall_accuracy'=overall_accuracy, 
+                                           'individual_accuracy'=individual_accuracy})
         return Instance(fields)
