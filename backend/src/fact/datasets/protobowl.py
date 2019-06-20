de7d29a01d0e1a6a664e292f1cbf6a17c609bd18
DenisPeskov <dap87@georgetown.edu>
2019-06-19 21:48:43 -0400

    Update protobowl.py
    where should original log be referenced?  And what/where should it write out?

--- backend/src/fact/datasets/protobowl.py
+++ backend/src/fact/datasets/protobowl.py
@@ -1,5 +1,6 @@
 import click
-
+import json
+from tqdm import tqdm_notebook
 
 @click.group(name='proto')
 def proto_cli():
@@ -8,6 +9,23 @@ def proto_cli():
 
 @proto_cli.command()
 def process():
-    """
-    TODO: Put the protobowl condenser code here
-    """
+    output = []
+    with open ('protobowl-042818.log') as f:
+        for line in f:
+            data = json.loads(line)
+            #there are 2k items with question_id instead of an actual id. Skip them.
+            if data['object']['qid'] == 'question_id':
+                continue
+            #time_remaining is ACTUAL total time in the original log
+            guess_ratio = round(data['object']['time_elapsed'] /data['object']['time_remaining'],4)
+            #pull out relevant variables
+            output.append({'qid':data['object']['qid'], 
+                           'user_id':data['object']['user']['id'], 
+                           'guess_ratio':guess_ratio, 
+                           'correct_answer_bool':data['object']['ruling'], 
+                           'date':data['date']}
+                         )
+            line = f.readline()
+
+    with open('protobowl-042818-condensed.log', 'w') as f:
+        json.dump({'data':output}, f)
