226d1f9adb0465bae19ba5d825a8c2282a16f45f
Wency16098574d <16098574d@connect.polyu.hk>
2019-10-01 20:25:09 +0800

    lr with pretrained bert embedding and 7 other features

--- backend/src/fact/datasets/qanta.py
+++ backend/src/fact/datasets/qanta.py
@@ -7,9 +7,12 @@ import math
 from tqdm import tqdm
 import click
 import numpy as np
+import pandas as pd
 from overrides import overrides
 from sklearn.model_selection import train_test_split
+from pytorch_pretrained_bert import BertTokenizer
 
+from sklearn.preprocessing import OneHotEncoder
 from allennlp.data import DatasetReader, TokenIndexer, Instance
 from allennlp.data.fields import TextField, LabelField, Field, MetadataField, ArrayField, ListField
 from allennlp.data.token_indexers import SingleIdTokenIndexer, TokenCharactersIndexer
@@ -24,11 +27,135 @@ from fact.util import get_logger
 
 log = get_logger(__name__)
 
-QANTA_TRAIN = 'data/expanded.qanta.train.2018.04.18.json'
-QANTA_DEV = 'data/expanded.qanta.dev.2018.04.18.json'
-QANTA_TEST = 'data/expanded.qanta.test.2018.04.18.json'
+TRAIN_RECORD = 'data/train.record.json'
+DEV_RECORD = 'data/dev.record.json'
+QUESTION_FILE = 'data/avgbert.question.json'
 
-USER_HASH = 'data/protobowl_byuser_hash.json'
+# TODO: Read this from data instead of hard code
+# Accuracy of users on questions
+# AVG_ACCURACY = .614
+# Average fraction of question text seen
+# AVG_FRAC_SEEN = 0.552
+
+def bert_embedding(df):
+    embedding = df['avg_bert']
+    qid =  list(df['qid'])
+    feature = dict(zip(qid, embedding))
+    feature['<UKN>'] = [0] * len(df['avg_bert'][0])
+    return feature
+
+def accuracy_per_user(df):
+    avg = df['ruling'].mean()
+    accuracy_series = df.groupby(['uid']).mean()['ruling']
+    value = []
+    for i in range(len(accuracy_series)):
+        value.append(accuracy_series[i])
+    uid =  list(accuracy_series.keys())
+    feature = dict(zip(uid, value))
+    feature['<UKN>'] = avg
+    return feature
+
+def average_buzz_ratio_per_user(df):
+    avg = df['buzz_ratio'].mean()
+    buzz_ratio_series = df.groupby(['uid']).mean()['buzz_ratio']
+    value = []
+    for i in range(len(buzz_ratio_series)):
+        value.append(buzz_ratio_series[i])
+    uid =  list(buzz_ratio_series.keys())
+    feature = dict(zip(uid, value))
+    feature['<UKN>'] = avg
+    return feature
+    
+def accuracy_per_question(df):
+    avg = df['ruling'].mean()
+    accuracy_series = df.groupby(['qid']).mean()['ruling']
+    value = []
+    for i in range(len(accuracy_series)):
+        value.append(accuracy_series[i])
+    qid =  list(accuracy_series.keys())
+    feature = dict(zip(qid, value))
+    feature['<UKN>'] = avg
+    return feature
+
+def average_buzz_ratio_per_question(df):
+    avg = df['buzz_ratio'].mean()
+    buzz_ratio_series = df.groupby(['qid']).mean()['buzz_ratio']
+    value = []
+    for i in range(len(buzz_ratio_series)):
+        value.append(buzz_ratio_series[i])
+    qid =  list(buzz_ratio_series.keys())
+    feature = dict(zip(qid, value))
+    feature['<UKN>'] = avg
+    return feature
+
+def uid_encoding(df):
+    uid_list = list(df.groupby('uid').groups.keys())
+    uid_list = [[uid] for uid in uid_list]
+    uid_enc = OneHotEncoder(handle_unknown='ignore')
+    uid_enc.fit(uid_list)
+    return uid_enc
+
+def qid_encoding(df):
+    qid_list = list(df.groupby('qid').groups.keys())
+    qid_list = [[qid] for qid in qid_list]
+    qid_enc = OneHotEncoder(handle_unknown='ignore')
+    qid_enc.fit(qid_list)
+    return qid_enc
+
+def uid_count(df):
+    uid = list(df.groupby('uid').groups.keys())
+    uid_count_series = df.groupby(['uid']).mean()['uid_count']
+    value = []
+    for i in range(len(uid_count_series)):
+        value.append(uid_count_series[i])
+    feature = dict(zip(uid, value))
+    feature['<UKN>'] = 0
+    return feature
+
+def qid_count(df):
+    qid = list(df.groupby('qid').groups.keys())
+    qid_count_series = df.groupby(['qid']).mean()['qid_count']
+    value = []
+    for i in range(len(qid_count_series)):
+        value.append(qid_count_series[i])
+    feature = dict(zip(qid, value))
+    feature['<UKN>'] = 0
+    return feature
+
+def times_seen(df):
+    cumcount_array = df.groupby(['uid', 'qid']).cumcount().values
+    feature = [[cumcount] for cumcount in cumcount_array]
+    return feature
+
+with open(TRAIN_RECORD) as f:
+    train_record = json.load(f)
+with open(DEV_RECORD) as f:
+    dev_record = json.load(f)
+with open(QUESTION_FILE) as f:
+    question_data = json.load(f)
+train_df = pd.DataFrame(train_record)
+dev_df = pd.DataFrame(dev_record)
+question_df = pd.DataFrame(question_data)
+
+embedding_dict = bert_embedding(question_df)
+accuracy_per_user_feature = accuracy_per_user(train_df)
+accuracy_per_question_feature = accuracy_per_question(train_df)
+average_buzz_ratio_per_user_feature = average_buzz_ratio_per_user(train_df)
+average_buzz_ratio_per_question_feature = average_buzz_ratio_per_question(train_df)
+
+# qid_enc = qid_encoding(train_df)
+# uid_enc = uid_encoding(train_df)
+# uid_list = [[record['uid']] for record in train_record]
+# qid_list = [[record['qid']] for record in train_record]
+# uid_feature = uid_enc.transform(uid_list)
+# qid_feature = qid_enc.transform(qid_list)
+
+uid_count_feature = uid_count(train_df)
+qid_count_feature = qid_count(train_df)
+
+train_times_seen_feature = times_seen(train_df)
+dev_times_seen_feature = times_seen(dev_df)
+
 
 @DatasetReader.register('qanta')
 class QantaReader(DatasetReader):
@@ -38,71 +165,115 @@ class QantaReader(DatasetReader):
                  token_indexers: Dict[str, TokenIndexer] = None):
         super().__init__(lazy)
         # guesstrain, guessdev, guesstest, buzztrain, buzzdev, buzztest
+        # self._tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
         self._tokenizer = tokenizer or WordTokenizer(SpacyWordSplitter())
-        # TODO: Add character indexer
+        # # TODO: Add character indexer
         self._token_indexers = token_indexers or {'tokens': SingleIdTokenIndexer()}
 
     @overrides
-    def _read(self, file_path):
-        #open the dict of by_user statistics
-        with open(USER_HASH) as user_file:
-            user_hash = json.load(user_file)['data']
-
-        #the following code opens an expanded QANTA file containing questions along with
-        #statistics for those questions.  It then sees what user is answering the question 
-        #and integrates relevant information for that user.
+    def _read(self, file_path:str):
         with open(file_path) as f:
-            for q in json.load(f)['questions']:
-                relevant_users = q['question_stats']['users_per_question']
-                for user in relevant_users:
-                    user_data = user_hash[user]
-                    if q['page'] is not None:
-                        #logic to calculate how much of question the user had seen
-                        index_of_user = q['question_stats']['users_per_question'].index(user)
-                        question_percent_seen = q['question_stats']['length_per_question'][index_of_user]
-                        index =  math.floor(len(q['text'] ) * question_percent_seen)
-                        seen_data = q['text'][0:index]
-                        #extract relevant features from the expanded QANTA data and the user data
-                        instance = self.text_to_instance(seen_data, answer=q['page'], qanta_id=q['qanta_id'],
-                                                             #question features
-                                                             length_per_question = q['question_stats']['length_per_question'],
-                                                             overall_length_per_question = q['question_stats']['overall_length_per_question'],
-                                                             overall_accuracy_per_question = q['question_stats']['overall_accuracy_per_question'],
-                                                             accuracy_per_question = q['question_stats']['accuracy_per_question'],
+            data = json.load(f)
 
-                                                             #user features
-                                                             length_per_user = user_data['length_per_user'],
-                                                             overall_length_per_user = user_data['overall_length_per_user'],
-                                                             accuracy_per_user = user_data['accuracy_per_user'],
-                                                             overall_accuracy_per_user = user_data['overall_accuracy_per_user']
-                                                             )
-                        if instance is not None:
-                             yield instance
+        num = len(data)
+        df = pd.DataFrame(data)
+
+        if file_path == 'data/train.record.json': 
+            times_seen_feature = train_times_seen_feature
+        else:
+            times_seen_feature = dev_times_seen_feature
+
+        for i in range(num):
+            uid = data[i]['uid']
+            qid = data[i]['qid']
+            text = question_df.loc[question_df['qid'] == qid]['text'].values
+            if uid in accuracy_per_user_feature:
+                user_accuracy = accuracy_per_user_feature[uid]
+                user_buzzratio = average_buzz_ratio_per_user_feature[uid]
+                uid_count = uid_count_feature[uid]
+            else:
+                user_accuracy = accuracy_per_user_feature['<UKN>']
+                user_buzzratio = average_buzz_ratio_per_user_feature['<UKN>']
+                uid_count = uid_count_feature['<UKN>']
+
+            if qid in accuracy_per_question_feature:
+                embedding = embedding_dict[qid]
+                question_accuracy = accuracy_per_question_feature[qid]
+                question_buzzratio = average_buzz_ratio_per_question_feature[qid]
+                qid_count = qid_count_feature[qid]
+            else:
+                embedding = embedding_dict['<UKN>']
+                question_accuracy = accuracy_per_question_feature['<UKN>']
+                question_buzzratio = average_buzz_ratio_per_question_feature['<UKN>']
+                qid_count = qid_count_feature['<UKN>']
+
+            feature_vec = [user_accuracy] + [user_buzzratio] + [uid_count] +  [question_accuracy] + [question_buzzratio] + [qid_count] + times_seen_feature[i]
+
+            label = data[i]['ruling']
+            instance = self.text_to_instance(
+                # tokens=[Token(word) for word in self._tokenizer(text.keys())],
+                tokens = [Token(word) for word in text],
+                embedding = embedding,
+                feature_vec = feature_vec,
+                label=label
+                )
+            # print("feature_vec", feature_vec)
+            # print("feature_vec len", len(feature_vec)
+            if file_path == 'data/train.record.json' and i > 2100000:
+                break
+            if file_path == 'data/dev.record.json' and i > 300000:
+                break
+            yield instance
+
+
     @overrides
     def text_to_instance(self,
-                         text: str,
-                         answer: str = None,
-                         qanta_id: int = None,
-                         length_per_question: List[float] = [1],
-                         overall_length_per_question: float = 1,
-                         accuracy_per_question: List[bool] = [True],
-                         overall_accuracy_per_question: float = 1,
-
+                         tokens: List[Token], 
+                        # answer: str,
+                        #  frac_seen: float = AVG_FRAC_SEEN,
+                        #  qid_encoding: np.ndarray,
+                         embedding: List[float],
+                         feature_vec: List[float],
+                        #  question_buzz_ratio: float,
                          # user features
-                         length_per_user: List[float] = [1],
-                         overall_length_per_user: float = 1,
-                         accuracy_per_user: List[bool] = [True],
-                         overall_accuracy_per_user: float = 1
-                        ):
-
-        fields: Dict[str, Field] = {}
-        tokenized_text = self._tokenizer.tokenize(text)
-        if len(tokenized_text) == 0:
-            return None
-        fields['text'] = TextField(tokenized_text, token_indexers=self._token_indexers)
-        if answer is not None:
-            fields['answer'] = LabelField(answer, label_namespace='answer_labels')
-        fields['metadata'] = MetadataField({'qanta_id': qanta_id})
-        fields['question_features'] = ArrayField(np.array([overall_length_per_question, overall_accuracy_per_question]))
-        fields['user_features'] = ArrayField(np.array([overall_length_per_user, overall_accuracy_per_user]))
+                        #  user_avg_frac_seen: float = AVG_FRAC_SEEN,
+                        #  user_avg_accuracy: float = AVG_ACCURACY,
+                        #  qanta_id: int = None,
+                         label: bool = None):
+        sentence_field = TextField(tokens, self._token_indexers)
+        fields: Dict[str, Field] = {"tokens": sentence_field}
+        # tokenized_text = self._tokenizer.tokenize(text)
+        # if len(tokenized_text) == 0:
+        #     return None
+        # fields['text'] = TextField(tokenized_text, token_indexers=self._token_indexers)
+        # fields['answer'] = LabelField(answer, label_namespace='answer_labels')
+        # fields['metadata'] = MetadataField({'qanta_id': qanta_id})
+        fields['embedding'] = ArrayField(np.array(embedding))
+        fields['feature_vec'] = ArrayField(np.array(feature_vec))
+        # fields['user_features'] = ArrayField(np.array([user_avg_frac_seen, user_avg_accuracy]))
+        if label is not None:
+            # fields['text'] = TextField(tokenized_text, token_indexers=self._token_indexers)
+            fields['label'] = LabelField(int(label), skip_indexing=True)
         return Instance(fields)
+
+    # def category_subcategory_difficulty(self, question_df):
+    #     df = question_df
+    #     print("question len: ", len(df.groupby('qid')))
+    #     enc = OneHotEncoder(handle_unknown='ignore')
+    #     category_list = list(df.groupby('category').groups.keys())
+    #     # subcategory_list = list(df.groupby('subcategory').groups.keys())
+    #     difficulty_list = list(df.groupby('difficulty').groups.keys())
+    #     category_subcategory_difficulty_list = []
+    #     sorted_len = [len(category_list), len(subcategory_list), len(difficulty_list)]
+    #     sorted_len.sort()
+    #     for i in range(sorted_len[-1]):
+    #         if i < sorted_len[0]:
+    #             category_subcategory_difficulty_list.append([category_list[i], subcategory_list[i], difficulty_list[i]])
+    #         elif i < sorted_len[1]:
+    #             category_subcategory_difficulty_list.append([category_list[i], subcategory_list[i], difficulty_list[0]])
+    #         else:
+    #             category_subcategory_difficulty_list.append([category_list[0], subcategory_list[i], difficulty_list[0]])
+    #     enc.fit(category_subcategory_difficulty_list)
+    #     return enc
+
+    
--- backend/src/fact/models/baseline.py
+++ backend/src/fact/models/baseline.py
@@ -1,19 +1,92 @@
+from typing import Dict
+import numpy as np
 import torch
-from allennlp.models import Model
+from torch import nn
+from allennlp.nn import InitializerApplicator
 from allennlp.data import Vocabulary
-
+from allennlp.models import Model
+from allennlp.modules import (
+    Seq2SeqEncoder, Seq2VecEncoder, 
+    TextFieldEmbedder
+)
+from allennlp.modules.seq2vec_encoders.boe_encoder import BagOfEmbeddingsEncoder
+from allennlp.modules.text_field_embedders import TextFieldEmbedder, BasicTextFieldEmbedder
+from allennlp.nn.util import get_text_field_mask
+from allennlp.modules.token_embedders import Embedding
+from allennlp.training.metrics import CategoricalAccuracy
 
 @Model.register('baseline')
 class Baseline(Model):
-    def __init__(self, vocab: Vocabulary):
+    def __init__(self, 
+                vocab: Vocabulary,
+                text_field_embedder: TextFieldEmbedder,
+                seq2vec_encoder: Seq2VecEncoder,
+                dropout: float,
+                num_labels: int = None,
+                initializer: InitializerApplicator = InitializerApplicator()):
         super().__init__(vocab)
-    
+
+        # EMBEDDING_DIM = 100
+        # token_embedding = Embedding(num_embeddings=vocab.get_vocab_size('tokens'),
+        #                     embedding_dim=EMBEDDING_DIM)
+        # word_embeddings = BasicTextFieldEmbedder({"tokens": token_embedding})
+        # self._text_field_embedder = word_embeddings
+        # self._seq2vec_encoder = seq2vec_encoder
+        # self._classifier_input_dim = self._seq2vec_encoder.get_output_dim() + 4
+        self._classifier_input_dim = 768 + 7
+        
+        # if dropout != 0:
+        #     self._dropout = nn.Dropout(dropout)
+        # else:
+        #     self._dropout = lambda x: x
+
+        self._num_labels = 2
+
+        # classification_layer = [nn.Linear(self._classifier_input_dim, self._num_labels)]
+        classification_layer = nn.Linear(self._classifier_input_dim, self._num_labels)
+        # if dropout != 0:
+        #     classification_layer.append(nn.Dropout(dropout))
+        # self._classification_layer = nn.Sequential(*classification_layer)
+        self._classification_layer = classification_layer
+        self._accuracy = CategoricalAccuracy()
+        self._loss = torch.nn.CrossEntropyLoss()
+        initializer(self)
+
     # Change these to match the text_to_instance argument names
-    def forward(self, *args, **kwargs):
+    def forward(self,
+                tokens: Dict[str, torch.Tensor],
+                # answer: torch.Tensor,
+                # metadata: Dict,
+                embedding: np.ndarray,
+                feature_vec: np.ndarray,
+                # user_features: np.ndarray,
+                label: torch.Tensor = None,
+                ):
         # This is where all the modeling stuff goes
         # AllenNLP requires that if there are training labels,
         # that a dictionary with key "loss" be returned.
         # You can stuff anything else in the dictionary that might be helpful
         # later.
-    
-        return {'loss': torch.ones(1)}
+        # embedded_text = self._text_field_embedder(text)
+        # mask = get_text_field_mask(text).float()
+        # embedded_text = self._dropout(embedded_text)
+        # embedded_text = self._seq2vec_encoder(embedded_text, mask=mask)
+        # # print("embedded_text: ", type(embedded_text), 'dim ', embedded_text.dim(), 'size', embedded_text.size())
+        # print("embedding", embedding.shape)
+        # print("feature_vec", feature_vec.shape)
+        encoding = torch.cat((embedding, feature_vec), dim=1)
+        # feature_vec = question_features
+        logits = self._classification_layer(encoding)
+        probs = torch.nn.functional.softmax(logits, dim=-1)
+        output_dict = {'logits': logits, 'probs': probs}
+
+        if label is not None:
+            self._accuracy(logits, label)
+            loss = self._loss(logits, label.long().view(-1))
+            output_dict['loss'] = loss
+
+        return output_dict
+
+    def get_metrics(self, reset: bool = False) -> Dict[str, float]:
+        metrics = {'accuracy': self._accuracy.get_metric(reset)}
+        return metrics 
