57e7802e5377a92894ee93ffb5f2dab65d0ffb4a
EntilZha <ski.rodriguez@gmail.com>
2019-10-30 02:18:49 -0400

    Add run scripts

--- backend/src/bert.sh
+++ backend/src/bert.sh
@@ -3,7 +3,7 @@
 #SBATCH --partition=gpu
 #SBATCH --time=4-00:00:00
 #SBATCH --gres=gpu:1
-#SBATCH --job-name=fact
+#SBATCH --job-name=fact-bert
 #SBATCH --cpus-per-task=4
 #SBATCH --mem-per-cpu=20g
 #SBATCH --chdir=/fs/clip-quiz/entilzha/code/fact-repetition/backend/src
