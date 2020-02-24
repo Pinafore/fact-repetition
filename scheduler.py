a290a0140f99adb4d6e339e6f6a40bb4b4e510b6
sjtufs <sjtufs@gmail.com>
2020-02-24 01:45:16 -0500

    new scheduler running fine

--- scheduler.py
+++ scheduler.py
@@ -55,13 +55,14 @@ class Scheduler(ABC):
 class MovingAvgScheduler(Scheduler):
 
     def __init__(self, n_components=20,
-                 lambda_embedding=0.1, lambda_prob=0.7, lambda_category=0.3,
+                 lambda_qrep=0.1, lambda_prob=0.7, lambda_category=0.3,
                  lambda_repetition=-1.0, lambda_leitner=1.0,
                  step_correct=0.5, step_wrong=0.05, step_qrep=0.3,
                  vectorizer_path='checkpoints/tf_vectorizer.pkl',
                  lda_path='checkpoints/lda.pkl'):
         self.n_components = n_components
-        self.lambda_embedding = lambda_embedding
+        self.lambda_qrep = lambda_qrep
+        self.lambda_prob = lambda_prob
         self.lambda_category = lambda_category
         self.lambda_repetition = lambda_repetition
         self.lambda_leitner = lambda_leitner
@@ -76,14 +77,7 @@ class MovingAvgScheduler(Scheduler):
             records_df = pickle.load(f)
         with open('data/jeopardy_358974_questions_20190612.pkl', 'rb') as f:
             questions_df = pickle.load(f)
-            # questions_df = questions_df.rename(columns={
-            #     'gameid': 'game_id',
-            #     'questionid': 'question_id',
-            #     'airdate': 'air_date',
-            #     'catnum': 'cat_num',
-            #     'clue': 'text',
-            # })
-        karl_to_question_id = questions_df.to_dict()['question_id']
+        self.karl_to_question_id = questions_df.to_dict()['question_id']
             
         # augment question_df with record stats
         print('merging dfs...')
@@ -117,6 +111,7 @@ class MovingAvgScheduler(Scheduler):
             self.lda = pickle.load(f)
 
         self.reset()
+        print('scheduler ready')
 
     def reset(self):
         # round number since start
@@ -130,7 +125,7 @@ class MovingAvgScheduler(Scheduler):
         self.category = 'HISTORY'
 
         # cache of embeddings
-        self.embed_cache = dict()
+        self.qrep_cache = dict()
         # cache of card probability
         self.prob_cache = dict()
         # cache of previous study round number
@@ -175,18 +170,18 @@ class MovingAvgScheduler(Scheduler):
 
     def embed(self, cards):
         for i, card in enumerate(cards):
-            cards[i]['embedding'] = self.embed_one(card)
+            cards[i]['qrep'] = self.embed_one(card)
         return cards
 
     def embed_one(self, card):
-        if 'embedding' in card:
-            self.embed_cache[card['question_id']] = card['embedding']
-            return card['embedding']
-        if card['question_id'] in self.embed_cache:
-            return self.embed_cache[card['question_id']]
-        embedding = lda.transform(tf_vectorizer.transform([card['text']]))
-        self.embed_cache[card['question_id']] = embedding
-        return embedding
+        if 'qrep' in card:
+            self.qrep_cache[card['question_id']] = card['qrep']
+            return card['qrep']
+        if card['question_id'] in self.qrep_cache:
+            return self.qrep_cache[card['question_id']]
+        qrep = self.lda.transform(self.tf_vectorizer.transform([card['text']]))
+        self.qrep_cache[card['question_id']] = qrep
+        return qrep
 
     def predict(self, cards):
         for i, card in enumerate(cards):
@@ -230,7 +225,7 @@ class MovingAvgScheduler(Scheduler):
 
     def _predict_one(self, card):
         # 1. find same or similar card in records
-        cards, score = self.retrieve(card)
+        cards, scores = self.retrieve(card)
         if len(cards) > 0:
             return np.dot([x['prob'] for x in cards], scores)
         # 2. use model to predict
@@ -238,7 +233,8 @@ class MovingAvgScheduler(Scheduler):
         return 0
 
     def set_params(self, params):
-        self.lambda_embedding = params.get('lambda_embedding', lambda_embedding)
+        self.lambda_qrep = params.get('lambda_qrep', lambda_qrep)
+        self.lambda_prob = params.get('lambda_prob', lambda_prob)
         self.lambda_category = params.get('lambda_category', lambda_category)
         self.lambda_repetition = params.get('lambda_repetition', lambda_repetition)
         self.lambda_leitner = params.get('lambda_leitner', lambda_leitner)
@@ -255,10 +251,10 @@ class MovingAvgScheduler(Scheduler):
         return int(card['category'] != self.category)
     
     def dist_prob(self, card):
-        if 'embedding' not in card:
-            card['embedding'] = self.embed_one(card)
-        topic_idx = np.argmax(card['embedding'])
-        d = card['prob'] - self.prob[topic_index]
+        if 'qrep' not in card:
+            card['qrep'] = self.embed_one(card)
+        topic_idx = np.argmax(card['qrep'])
+        d = card['prob'] - self.prob[topic_idx]
         # penalize easier questions
         d *= 1 if d < 0 else 10
         return abs(d)
@@ -266,7 +262,7 @@ class MovingAvgScheduler(Scheduler):
     def dist_qrep(self, card):
         def cosine_distance(a, b):
             return 1 - (a @ b.T) / (np.linalg.norm(a) * np.linalg.norm(b))
-        return cosine_distance(self.embedding, card['embedding'])
+        return cosine_distance(self.qrep, card['qrep'])
 
     def dist_leitner(self, card):
         # TODO
@@ -276,9 +272,9 @@ class MovingAvgScheduler(Scheduler):
         # compute distance metric of given card to current state
         # dist = topic repr dist + topical difficulty dist + recency + leitner score
         # NOTE lower is better
-        card['embedding'] = self.embed_one(card)
+        card['qrep'] = self.embed_one(card)
         card['prob'] = self.predict_one(card)
-        return self.lambda_embedding * self.dist_qrep(card) \
+        return self.lambda_qrep * self.dist_qrep(card) \
             + self.lambda_prob * self.dist_prob(card) \
             + self.lambda_category * self.dist_category(card) \
             # + self.lambda_repetition * self.dist_rep(card)
@@ -294,10 +290,10 @@ class MovingAvgScheduler(Scheduler):
             # NOTE lower is better
             cards[i]['score'] = scores[i]
         reverse_index = {x['question_id']: i for i, x in enumerate(cards)}
-        cards_sorted = sorted(cards, key=lambda x: x['dist'])
+        cards_sorted = sorted(cards, key=lambda x: x['score'])
         # sorted indices 
         order = [reverse_index[x['question_id']] for x in cards_sorted]
-        ranking = [order.index(i) for i, _ in cards]
+        ranking = [order.index(i) for i, _ in enumerate(cards)]
         return order, ranking
 
     def update(self, cards):
@@ -308,23 +304,22 @@ class MovingAvgScheduler(Scheduler):
                 a = self.step_correct
                 self.prob[topic_index] = a * card['prob'] + (1 - a) * self.prob[topic_index]
             else:
-                self.prob['prob'][topic_index] += self.step_wrong
+                self.prob[topic_index] += self.step_wrong
             self.prob[topic_index] = min(1.0, self.prob[topic_index])
             b = self.step_qrep
-            self.embedding = b * card['embedding'] + (1 - b) * card['embedding']
+            self.qrep = b * card['qrep'] + (1 - b) * self.qrep
             self.category = card['category']
             
     
 class Hyperparams(BaseModel):
-    qrep: Optional[float]
-    prob_difficult: Optional[float]
-    prob_easy: Optional[float]
-    category: Optional[float]
-    repetition: Optional[float]
-    leitner: Optional[float]
-    lr_prob_correct: Optional[float]
-    lr_prob_wrong: Optional[float]
-    lr_qrep: Optional[float]
+    lambda_qrep: Optional[float]
+    lambda_prob: Optional[float]
+    lambda_category: Optional[float]
+    lambda_repetition: Optional[float]
+    lambda_leitner: Optional[float]
+    step_correct: Optional[float]
+    step_wrong: Optional[float]
+    step_qrep: Optional[float]
 
 
 app = FastAPI()
@@ -333,13 +328,15 @@ scheduler = MovingAvgScheduler()
 
 @app.post('/api/karl/predict')
 def karl_predict(card: Flashcard):
-    scheduler.predict_one(card.dict())
+    score = scheduler.predict_one(card.dict())
+    return {
+        'prob': score
+    }
 
 @app.post('/api/karl/schedule')
 def karl_schedule(cards: List[Flashcard]):
     order, ranking = scheduler.schedule([x.dict() for x in cards])
     return {
-        'all_labels': [['correct', 'wrong'] for x in flashcards],
         'order': order,
         'ranking': ranking,
     }
