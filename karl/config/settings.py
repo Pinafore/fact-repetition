e4df3bda93b67fdb27319142e7f0dfb3273321e2
sjtufs <sjtufs@gmail.com>
2020-12-20 11:23:47 -0500

    use defult unix_socket_directories

--- karl/config/settings.py
+++ karl/config/settings.py
@@ -1,7 +1,6 @@
 CODE_DIR = '/fs/clip-quiz/shifeng/karl-dev'
 DATA_DIR = f'{CODE_DIR}/data'
-DB_HOST = '/scratch0/shifeng/karl-db/run'
-SQLALCHEMY_DATABASE_URL = f'postgresql+psycopg2://shifeng@localhost:5433/karl-dev?host={DB_HOST}'
+SQLALCHEMY_DATABASE_URL = 'postgresql+psycopg2://shifeng@localhost:5433/karl-prod'
 STABLE_DATABASE_URL = 'postgresql+psycopg2://shifeng@localhost:5434/karl-prod'
 USE_MULTIPROCESSING = True
 USER_STATS_CACHE = True
