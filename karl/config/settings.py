27f0fc9dd9a3ebd000f847a9609f5a1f6a84bf74
sjtufs <sjtufs@gmail.com>
2020-12-20 11:28:01 -0500

    updte dir

--- karl/config/settings.py
+++ karl/config/settings.py
@@ -1,6 +1,6 @@
 CODE_DIR = '/fs/clip-quiz/shifeng/karl-dev'
 DATA_DIR = f'{CODE_DIR}/data'
-SQLALCHEMY_DATABASE_URL = 'postgresql+psycopg2://shifeng@localhost:5433/karl-prod'
+SQLALCHEMY_DATABASE_URL = 'postgresql+psycopg2://shifeng@localhost:5433/karl-dev'
 STABLE_DATABASE_URL = 'postgresql+psycopg2://shifeng@localhost:5434/karl-prod'
 USE_MULTIPROCESSING = True
 USER_STATS_CACHE = True
