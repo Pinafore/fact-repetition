d2b12502e00eb6ca69d77f4090597bf31a6f1aff
ihsgnef <shifeng@cs.umd.edu>
2021-03-12 17:30:55 -0500

    update

--- karl/config/settings.py
+++ karl/config/settings.py
@@ -1,7 +1,7 @@
-# CODE_DIR = '/fs/clip-quiz/shifeng/karl-dev'
-CODE_DIR = '/Users/shifeng/workspace/karl-dev'
+CODE_DIR = '/fs/clip-quiz/shifeng/karl-dev'
+# CODE_DIR = '/Users/shifeng/workspace/karl-dev'
 DATA_DIR = f'{CODE_DIR}/data'
-SQLALCHEMY_DATABASE_URL = 'postgresql+psycopg2://shifeng@localhost:5433/karl-dev'
+SQLALCHEMY_DATABASE_URL = 'postgresql+psycopg2://shifeng@localhost:5433/karl-prod'
 STABLE_DATABASE_URL = 'postgresql+psycopg2://shifeng@localhost:5433/karl-prod'
 USE_MULTIPROCESSING = True
 USER_STATS_CACHE = True
