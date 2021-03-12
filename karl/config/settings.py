d9a10c931b840f7c7b21c7077564203877e068f4
sjtufs <sjtufs@gmail.com>
2021-03-11 22:43:18 -0500

    update

--- karl/config/settings.py
+++ karl/config/settings.py
@@ -1,7 +1,7 @@
 CODE_DIR = '/fs/clip-quiz/shifeng/karl-dev'
 DATA_DIR = f'{CODE_DIR}/data'
-SQLALCHEMY_DATABASE_URL = 'postgresql+psycopg2://shifeng@localhost:5433/karl-dev'
-STABLE_DATABASE_URL = 'postgresql+psycopg2://shifeng@localhost:5434/karl-prod'
+SQLALCHEMY_DATABASE_URL = 'postgresql+psycopg2://shifeng@localhost:5432/karl-dev'
+STABLE_DATABASE_URL = 'postgresql+psycopg2://shifeng@localhost:5432/karl-prod'
 USE_MULTIPROCESSING = True
 USER_STATS_CACHE = True
 MP_CONTEXT = 'fork'
--- karl/tests/test_scheduling.py
+++ karl/tests/test_scheduling.py
@@ -20,7 +20,7 @@ n_facts_per_query = 300
 user_id = 'dummy'
 start_date = parse_date('2028-06-01 08:00:00.000001 -0400')
 
-# requests.get(f'{URL}/reset_user?env=prod&user_id={user_id}')
+requests.get(f'{URL}/reset_user?env=prod&user_id={user_id}')
 requests.put(f'{URL}/set_params?env=prod&user_id={user_id}', data=json.dumps(ParametersSchema().__dict__))
 
 profile = {}  # key -> [values]
