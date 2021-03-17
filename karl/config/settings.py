75a6ca493fa03044bf7d91dbb7ff1556403a560f
sjtufs <sjtufs@gmail.com>
2021-03-17 15:51:18 -0400

    update

--- karl/config/settings.py
+++ karl/config/settings.py
@@ -1,5 +1,5 @@
-# CODE_DIR = '/fs/clip-quiz/shifeng/karl-dev'
-CODE_DIR = '/Users/shifeng/workspace/karl-dev'
+CODE_DIR = '/fs/clip-quiz/shifeng/karl-dev'
+# CODE_DIR = '/Users/shifeng/workspace/karl-dev'
 DATA_DIR = f'{CODE_DIR}/data'
 SQLALCHEMY_DATABASE_URL = 'postgresql+psycopg2://shifeng@localhost:5433/karl-prod'
 STABLE_DATABASE_URL = 'postgresql+psycopg2://shifeng@localhost:5433/karl-prod'
