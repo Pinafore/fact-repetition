d1a97395f270d0c495ae45e04790b671efb58560
ihsgnef <shifeng@cs.umd.edu>
2023-09-30 21:52:41 -0400

    add alembic init script

--- alembic/env.py
+++ alembic/env.py
@@ -5,8 +5,6 @@ from sqlalchemy import pool
 
 from alembic import context
 
-from karl.db.base import Base
-
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
@@ -20,6 +18,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
+from karl.db.base_class import Base
 target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
